57fb8d3220783fed7b0b3374c5abfe306b884893
MarcusO17 <marcusonggeye@gmail.com>
2025-06-22 05:08:14 +0000

    refactor MPI code comments for clarity and add new lab1.c file

--- Assignment/MPI/Task1/t1.c
+++ Assignment/MPI/Task1/t1.c
@@ -1,20 +1,20 @@
-#include <mpi.h>   // MPI_Init, MPI_Comm_size, MPI_Comm_rank, MPI_Finalize, MPI_COMM_WORLD
-#include <stdio.h> // printf
+#include <mpi.h>   
+#include <stdio.h> 
 
 int main(int argc, char *argv[]) {
     int rank; // The rank (ID) of the current process
-    int size; // The total number of processes in the communicator
+    int size; // The total number of processes in the MPI env
 
 
-    MPI_Init(&argc, &argv);
+    MPI_Init(&argc, &argv); //init mpi environment
 
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_size(MPI_COMM_WORLD, &size); // passes the number of processes into size
 
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank); // passes the rank of the current process into rank
 
     printf("Hello from process %d of %d!\n", rank, size);
 
-    MPI_Finalize();
+    MPI_Finalize(); // Finalize the MPI environment
 
     return 0;
 }
